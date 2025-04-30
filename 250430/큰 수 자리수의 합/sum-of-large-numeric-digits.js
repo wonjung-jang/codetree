0ffd24b91e1f67ab08a1401aed9786f7ac7601c6
Wonjung Jang <114918904+wonjung-jang@users.noreply.github.com>
2025-04-30 12:24:47 +0900

    Title: 큰 수 자리수의 합, Time: 163ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250430/큰 수 자리수의 합/sum-of-large-numeric-digits.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+let [a, b, c] = input[0].split(" ").map(Number);
+
+// Please Write your code here.
+const f = (n) => {
+    if(n < 10) return n;
+
+    return f(parseInt(n / 10)) + f(n % 10);
+}
+
+console.log(f(a * b * c));
