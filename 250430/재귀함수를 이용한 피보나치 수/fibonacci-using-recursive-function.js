90b3cc391fa09620588a6846edfa76c61e5f389d
Wonjung Jang <114918904+wonjung-jang@users.noreply.github.com>
2025-04-30 11:02:47 +0900

    Title: 재귀함수를 이용한 피보나치 수, Time: 198ms, Memory: 15MB, Status: Passed - Codetree

--- 250430/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.js
+++ 250430/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.js
@@ -3,11 +3,15 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 // Please Write your code here.
 const fib = (n) => {
+    if(n === 1){
+        return 1;
+    }
+
     if(n === 2){
         return 1;
     }
     
-    return 1 + fib(n - 1);
+    return fib(n - 1) + fib(n - 2);
 }
 
 console.log(fib(n));
