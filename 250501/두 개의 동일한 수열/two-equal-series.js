1fa547f92e5080586f554f30a6ba1e77fcebd55f
Wonjung Jang <114918904+wonjung-jang@users.noreply.github.com>
2025-05-01 10:16:43 +0900

    Title: 두 개의 동일한 수열, Time: 157ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250501/두 개의 동일한 수열/two-equal-series.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const a = input[1].trim().split(' ').map(Number);
+const b = input[2].trim().split(' ').map(Number);
+
+// Please Write your code here.
+a.sort((a, b) => a - b);
+b.sort((a, b) => a - b);
+
+let result = "Yes";
+for(let i = 0; i < n; i++){
+    if(a[i] !== b[i]) {
+        result = "No";
+    }
+}
+console.log(result);
