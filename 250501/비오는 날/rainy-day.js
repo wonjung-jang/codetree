0f6445d5a57b49615de9c7d28ba7e7770a0cf89a
Wonjung Jang <114918904+wonjung-jang@users.noreply.github.com>
2025-05-01 11:50:18 +0900

    Title: 비오는 날, Time: 168ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250501/비오는 날/rainy-day.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const forecasts = input.slice(1, n + 1).map(line => line.split(' '));
+
+// Please Write your code here.
+class Weather{
+    constructor(date, day, weather){
+        this.date = date;
+        this.day = day;
+        this.weather = weather;
+    }
+
+    get info(){
+        return `${this.date} ${this.day} ${this.weather}`;
+    }
+}
+
+const result = forecasts.map(forecast => {
+    const [date, day, weather] = forecast;
+    return new Weather(date, day, weather);
+}).find(forecast => forecast.weather === "Rain").info;
+
+console.log(result);
