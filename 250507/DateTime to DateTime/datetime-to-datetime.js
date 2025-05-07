debd821358ec89b38d74c4fb6e671269bfa27b84
Wonjung Jang <114918904+wonjung-jang@users.noreply.github.com>
2025-05-07 10:13:58 +0900

    Title: DateTime to DateTime, Time: 180ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250507/DateTime to DateTime/datetime-to-datetime.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [A, B, C] = input[0].split(' ').map(Number);
+
+// Please write your code here.
+const sumMin = (date, hour, min) => {
+    return (date * 24 * 60) + (hour * 60) + min;
+}
+
+console.log(sumMin(A - 11, B - 11, C - 11));
