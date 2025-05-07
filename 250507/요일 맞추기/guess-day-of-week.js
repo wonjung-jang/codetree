22dd4f2999c369047ceb855bff6893c6e03f98fd
Wonjung Jang <114918904+wonjung-jang@users.noreply.github.com>
2025-05-07 10:30:19 +0900

    Title: 요일 맞추기, Time: 186ms, Memory: 15MB, Status: Wrong Answer - Codetree

--- 250507/요일 맞추기/guess-day-of-week.js
+++ 250507/요일 맞추기/guess-day-of-week.js
@@ -11,5 +11,9 @@ const calcTotalDate = (month, date) => {
     return sum - d;
 }
 
-const diff = (calcTotalDate(m2, d2) - calcTotalDate(m1, d1)) % 7;
-console.log(day[1 + diff]);
+const diff = calcTotalDate(m2, d2) - calcTotalDate(m1, d1);
+if(diff > 0){
+    console.log(day[1 + diff % 7]);
+}else{
+    console.log(day[1 - Math.abs(diff) % 7]);
+}
