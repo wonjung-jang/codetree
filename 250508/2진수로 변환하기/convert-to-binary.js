2554ffc8d0ad2fb852f06b31cea792d0fc01a449
Wonjung Jang <114918904+wonjung-jang@users.noreply.github.com>
2025-05-08 09:17:01 +0900

    Title: 2진수로 변환하기, Time: 178ms, Memory: 15MB, Status: Passed - Codetree

--- 250508/2진수로 변환하기/convert-to-binary.js
+++ 250508/2진수로 변환하기/convert-to-binary.js
@@ -3,6 +3,7 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 // Please Write your code here.
 const convertBinaryNumber = (number) => {
+    if(number === 0) return 0;
     let result = [];
     while(number > 0){
         result.push(number % 2);
