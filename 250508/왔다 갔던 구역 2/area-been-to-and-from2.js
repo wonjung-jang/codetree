d363d0e7d930c4181996de7a15b51dc5752bfb43
Wonjung Jang <114918904+wonjung-jang@users.noreply.github.com>
2025-05-08 10:49:48 +0900

    Title: 왔다 갔던 구역 2, Time: 182ms, Memory: 15MB, Status: Wrong Answer - Codetree

--- 250508/왔다 갔던 구역 2/area-been-to-and-from2.js
+++ 250508/왔다 갔던 구역 2/area-been-to-and-from2.js
@@ -5,18 +5,21 @@ const n = Number(input[0]);
 const commands = input.slice(1, n + 1);
 
 // Please Write your code here.
-const offset = 1000;
-const section = Array.from({length: 2000}, () => 0);
+const section = {};
 
-let position = offset;
+let position = 0;
 for(let i = 0; i < n; i++){
     let [num, direct] = commands[i].split(" ");
     num = Number(num);
     while(num > 0){
-        position = direct === "R" ? position + 1 : position - 1;
-        section[position]++;
         num--;
+        position = direct === "R" ? position + 1 : position - 1;
+        if(!section[position]){
+            section[position] = 1;
+            continue;
+        }
+        section[position]++;
     }
 }
-
-console.log(section.filter(num => num >= 2).length);
+// Object.entries(section).forEach(([key, value]) =>console.log(key, value));
+console.log(Object.values(section).filter(num => num >= 2).length);
