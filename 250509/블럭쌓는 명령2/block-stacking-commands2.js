29f593d3ae699b769b8c31b1a3179c889e1fcad8
Wonjung Jang <114918904+wonjung-jang@users.noreply.github.com>
2025-05-09 13:07:04 +0900

    Title: 블럭쌓는 명령2, Time: 190ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250509/블럭쌓는 명령2/block-stacking-commands2.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const [n, k] = input[0].split(' ').map(Number);
+const segments = input.slice(1, k + 1).map(line => line.split(' ').map(Number));
+
+// Please write your code here.
+const kans = Array.from({length: n + 1}, () => 0);
+
+for(let i = 0; i < k; i++){
+    let [from, to] = segments[i];
+    while(from <= to){
+        kans[from]++;
+        from++;
+    }
+}
+
+console.log(Math.max(...kans));
