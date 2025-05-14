3cbdc720b3bf39d25c9f2a93faff3188853bc0d2
Wonjung Jang <114918904+wonjung-jang@users.noreply.github.com>
2025-05-14 11:18:24 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 857ms, Memory: 61MB, Status: Passed - Codetree

--- 250514/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.js
+++ 250514/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.js
@@ -6,24 +6,19 @@ const rectB = input[1].split(' ').map(Number);
 const rectM = input[2].split(' ').map(Number);
 
 // Please Write your code here.
-const fieldMap = {};
+const arr = Array.from({length: 2000}, () => Array.from({length: 2000}, () => 0));
+const offset = 1000;
 
 for(let num = 0; num < 2; num++){
     const [startX, startY, endX, endY] = [rectA, rectB][num];
 
     for(let i = startX; i < endX; i++){
         for(let j = startY; j < endY; j++){
-            fieldMap[`${i}, ${j}`] = 1;
+            const [minX, minY, maxX, maxY] = rectM;
+            if(i >= minX && i < maxX && j >= minY && j < maxY) continue;
+            arr[i + offset][j + offset] = 1;
         }
     }
 }
 
-const [startX, startY, endX, endY] = rectM;
-
-for(let i = startX; i < endX; i++){
-    for(let j = startY; j < endY; j++){
-        delete fieldMap[`${i}, ${j}`];
-    }
-}
-
-console.log(Object.values(fieldMap).length);
+console.log(arr.reduce((acc, cur) => acc += cur.filter(num => num === 1).length, 0));
