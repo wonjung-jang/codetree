64df2f8f6858e321c8d92e2f06ce6d5e4d89df0e
Wonjung Jang <114918904+wonjung-jang@users.noreply.github.com>
2025-05-14 14:27:34 +0900

    Title: 계속 중첩되는 사각형, Time: 190ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250514/계속 중첩되는 사각형/continuously-overlapping-squares.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+const rectangles = [];
+for (let i = 1; i <= n; i++) {
+    const [x1, y1, x2, y2] = input[i].split(' ').map(Number);
+    rectangles.push([x1, y1, x2, y2]);
+}
+// Please Write your code here.
+const offset = 100;
+const arr = Array.from({length: 200}, () => Array.from({length: 200}, () => 0));
+for(let i = 0; i < n; i++){
+    const [x1, y1, x2, y2] = rectangles[i];
+
+    for(let i = x1; i <= x2; i++){
+        for(let j = y1; j <= y2; j++){
+            if(i % 2 === 0){
+                arr[i + offset][j + offset] = 0;    
+            }else{
+                arr[i + offset][j + offset] = 1;
+            }
+        }
+    }
+}
+
+const result = arr.reduce((acc, cur) => acc += cur.filter(num => num === 1).length, 0);
+console.log(result);
